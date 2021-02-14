80a7e73d1111e033f302b67a6614203d29e3e62d
Lonami Exo <totufals@hotmail.com>
2021-02-14 13:44:00 +0100

    Send etag and If-Modified in headers to reduce server load

--- src/feed.rs
+++ src/feed.rs
@@ -1,21 +1,67 @@
-use std::collections::HashSet;
-use std::fmt;
+use chrono::{DateTime, Utc};
+use reqwest::header;
+use std::time::Duration;
+use std::{collections::HashSet, fmt};
+use tokio::time::Instant;
+
+/// Maximum cache delay we're willing to accept.
+///
+/// A bad-behaved server might put an absurd amount for the `max-age`, and then we would never
+/// check that feed again.
+const MAX_FETCH_DELAY: Duration = Duration::from_secs(24 * 60 * 60);
+
+/// If the server does not have any max age or expiration for the feed, use a default delay.
+const DEFAULT_FETCH_DELAY: Duration = Duration::from_secs(10 * 60);
 
 struct Feed {
     url: String,
     users: Vec<i32>,
     seen_entries: HashSet<String>,
+    last_fetch: DateTime<Utc>,
+    next_fetch: Instant,
+    etag: Option<String>,
 }
 
 #[derive(Debug)]
 enum Error {
     ReadError(reqwest::Error),
     ParseError(feed_rs::parser::ParseFeedError),
+    MalformedHeader(header::HeaderName),
+}
+
+fn header(headers: &header::HeaderMap, key: header::HeaderName) -> Result<Option<&str>, Error> {
+    Ok(match headers.get(&key) {
+        Some(v) => Some(v.to_str().map_err(|_| Error::MalformedHeader(key))?),
+        None => None,
+    })
+}
+
+fn find_expiry(headers: &header::HeaderMap) -> Result<Instant, Error> {
+    let now = Utc::now();
+    let delay = if let Some(_cache_control) = header(headers, header::CACHE_CONTROL)? {
+        todo!()
+    } else if let Some(expiry) = header(headers, header::EXPIRES)? {
+        let expires = DateTime::parse_from_rfc2822(expiry)
+            .map(DateTime::<Utc>::from)
+            .map_err(|_| Error::MalformedHeader(header::EXPIRES))?;
+
+        // If expiry date is less than now, `std::time::Duration` cannot represent it.
+        // This means that the server time is either wrong or we took too long to fetch.
+        (expires - now).to_std().unwrap_or(DEFAULT_FETCH_DELAY)
+    } else {
+        DEFAULT_FETCH_DELAY
+    };
+
+    Ok(Instant::now() + delay.min(MAX_FETCH_DELAY))
 }
 
 impl Feed {
     pub async fn new(http: &reqwest::Client, url: &str, user_id: i32) -> Result<Self, Error> {
-        let xml = http.get(url).send().await?.bytes().await?;
+        let resp = http.get(url).send().await?;
+        let last_fetch = Utc::now();
+        let next_fetch = find_expiry(resp.headers())?;
+        let etag = header(resp.headers(), header::ETAG)?.map(String::from);
+        let xml = resp.bytes().await?;
 
         let feed = feed_rs::parser::parse(xml.as_ref())?;
         let seen_entries = feed
@@ -28,15 +74,26 @@ impl Feed {
             url: url.to_string(),
             users: vec![user_id],
             seen_entries,
+            last_fetch,
+            next_fetch,
+            etag,
         })
     }
 
     pub async fn check(
         &mut self,
         http: &reqwest::Client,
-        url: &str,
     ) -> Result<Vec<feed_rs::model::Entry>, Error> {
-        let xml = http.get(url).send().await?.bytes().await?;
+        let mut request = http
+            .get(&self.url)
+            .header(header::IF_MODIFIED_SINCE, self.last_fetch.to_rfc2822());
+
+        if let Some(etag) = self.etag.as_ref() {
+            request = request.header(header::IF_NONE_MATCH, etag);
+        }
+
+        let xml = request.send().await?.bytes().await?;
+
         let mut feed = feed_rs::parser::parse(xml.as_ref())?;
         feed.entries
             .retain(|entry| !self.seen_entries.contains(&entry.id));
