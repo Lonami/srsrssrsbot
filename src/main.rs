e5b96c3ca0676bc80eafa29ac94ad5502d10856d
Lonami Exo <totufals@hotmail.com>
2021-02-14 18:57:33 +0100

    Use tokio::join to step over updates and network

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod feed;
 
-use grammers_client::{Client, Config, Update};
+use grammers_client::{Client, ClientHandle, Config, Update};
 use grammers_session::FileSession;
 use log;
+use mpsc::{Sender, UnboundedSender};
 use simple_logger::SimpleLogger;
 use std::collections::{BinaryHeap, HashSet};
+use tokio::sync::mpsc;
 
 static LOG_LEVEL: &str = env!("LOG_LEVEL");
 
@@ -40,62 +42,29 @@ fn str_add_err(url: &str, e: feed::Error) -> String {
     format!("Failed to add {} to your list of feeds: {}.", url, e)
 }
 
-#[tokio::main]
-async fn main() {
-    SimpleLogger::new()
-        .with_level(match LOG_LEVEL {
-            "ERROR" => log::LevelFilter::Error,
-            "WARN" => log::LevelFilter::Warn,
-            "INFO" => log::LevelFilter::Info,
-            "DEBUG" => log::LevelFilter::Debug,
-            "TRACE" => log::LevelFilter::Trace,
-            _ => log::LevelFilter::Off,
-        })
-        .init()
-        .unwrap();
-
-    let mut feeds = BinaryHeap::new();
-
-    let http = reqwest::Client::new();
-
-    let api_id = TG_API_ID.parse().unwrap();
-    let mut client = Client::connect(Config {
-        session: FileSession::load_or_create(SESSION_NAME).unwrap(),
-        api_id,
-        api_hash: TG_API_HASH.to_string(),
-        params: Default::default(),
-    })
-    .await
-    .unwrap();
-
-    if !client.is_authorized().await.unwrap() {
-        client
-            .bot_sign_in(BOT_TOKEN, api_id, TG_API_HASH)
-            .await
-            .unwrap();
-        client.session().save().unwrap();
-    }
-
-    let mut tg = client.handle();
-
-    // Need the `client` to be stepping the network, or the handle methods will never complete.
-    let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
-    tokio::task::spawn(async move {
-        loop {
-            match client.next_updates().await {
-                Ok(Some(updates)) => {
-                    updates.for_each(|update| tx.send(update).map_err(drop).unwrap());
-                }
-                Ok(None) => {
-                    break;
-                }
-                Err(e) => {
-                    eprintln!("Error reading updates: {}", e);
-                }
+async fn step_network(
+    mut tg: Client<FileSession>,
+    tx: mpsc::UnboundedSender<Update>,
+) -> Client<FileSession> {
+    loop {
+        match tg.next_updates().await {
+            Ok(Some(updates)) => {
+                updates.for_each(|update| tx.send(update).map_err(drop).unwrap());
+            }
+            Ok(None) => {
+                break;
+            }
+            Err(e) => {
+                eprintln!("Error reading updates: {}", e);
             }
         }
-        client
-    });
+    }
+    tg
+}
+
+async fn step_updates(mut tg: ClientHandle, mut rx: mpsc::UnboundedReceiver<Update>) {
+    let http = reqwest::Client::new();
+    let mut feeds = BinaryHeap::new();
 
     while let Some(update) = rx.recv().await {
         match update {
@@ -138,4 +107,43 @@ async fn main() {
             _ => {}
         };
     }
+}
+
+#[tokio::main]
+async fn main() {
+    SimpleLogger::new()
+        .with_level(match LOG_LEVEL {
+            "ERROR" => log::LevelFilter::Error,
+            "WARN" => log::LevelFilter::Warn,
+            "INFO" => log::LevelFilter::Info,
+            "DEBUG" => log::LevelFilter::Debug,
+            "TRACE" => log::LevelFilter::Trace,
+            _ => log::LevelFilter::Off,
+        })
+        .init()
+        .unwrap();
+
+    let api_id = TG_API_ID.parse().unwrap();
+    let mut client = Client::connect(Config {
+        session: FileSession::load_or_create(SESSION_NAME).unwrap(),
+        api_id,
+        api_hash: TG_API_HASH.to_string(),
+        params: Default::default(),
+    })
+    .await
+    .unwrap();
+
+    if !client.is_authorized().await.unwrap() {
+        client
+            .bot_sign_in(BOT_TOKEN, api_id, TG_API_HASH)
+            .await
+            .unwrap();
+        client.session().save().unwrap();
+    }
+
+    let mut tg = client.handle();
+
+    // Need the `client` to be stepping the network, or the handle methods will never complete.
+    let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
+    tokio::join!(step_updates(client.handle(), rx), step_network(client, tx));
 }
